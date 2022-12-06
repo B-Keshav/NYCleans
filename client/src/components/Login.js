e46f490d4c03ebb2ea05137d549a01c86f2fbe9a
angpace <108838753+angpace@users.noreply.github.com>
2022-12-06 11:12:02 -0500

    components folder

---
+++ client/src/components/Login.js
@@ -0,0 +1,14 @@
+import UserProfile from "./UserProfile";
+
+function Login () {
+
+    return (
+        <> 
+        <h1>Please log in</h1> 
+        < UserProfile/> 
+        </>
+
+    )
+}
+
+export default Login;
---
+++ client/src/components/UserProfile.js
@@ -0,0 +1,9 @@
+function UserProfile () {
+
+
+    return (
+        <h1>You logged in</h1> 
+    )
+}
+
+export default UserProfile;
