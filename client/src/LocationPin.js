dd6b9886383ad65d268712ce58b3ba6be2332b4d
veryharrywilz <110489443+veryharrywilz@users.noreply.github.com>
2022-12-05 20:55:21 -0500

    Added map

---
+++ client/src/LocationPin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import locationIcon from './images/LocationIcon.png'
+
+
+function LocationPin () {
+  <div className="pin">
+    <img src={locationIcon} className="pin-icon" />
+    <p className="pin-text">Address</p>
+  </div>
+}
+
+export default LocationPin;
--- client/src/Map.js
+++ client/src/Map.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import LocationPin from "./LocationPin";
+import './map.css'
+import GoogleMapReact from 'google-map-react'
 
 function Map() {
+    const [jobLocations, setJobLocations] = useState([])
+    const location = {
+        address: '11 Broadway, New York City, NY 10004',
+        lat: 40.70521926879883,
+        lng: -74.01383209228516,
+    }
+
+    useEffect(() => {
+        fetch("/joblocations")
+          .then((r) => r.json())
+          .then(data => setJobLocations(data))
+      }, []);
+
+      const jobLocationArray = jobLocations.map((location => {
+        <LocationPin
+        lat={location.lat}
+        lng={location.lng}
+        text={location.address}
+        />
+      }))
+
     return (
-        <h1>Map going here</h1>
+        <div className="map">
+            <h2 className="map-h2">Come Visit Us At Our Campus</h2>
+
+            <div className="google-map">
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: 'AIzaSyB6wyOfDyMZwASobvaG-XhnmGLyzx2zYxo' }}
+                    defaultCenter={location}
+                    defaultZoom={17}
+                >
+                    {jobLocationArray}
+                </GoogleMapReact>
+            </div>
+        </div>
     )
 }
 
