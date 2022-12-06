4bb204e4c2f88c42c3b4d32b1e177d50982d8207
angpace <108838753+angpace@users.noreply.github.com>
2022-12-06 14:19:01 -0500

    Creating an account

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,13 +4,12 @@ import UserProfile from "./UserProfile";
 import {useState} from "react";
 
 function Login () {
-        const [user, createUser] = useState({
-            name: "",
-            age: 0,
-            password: "",
-            bio: "",
-            organization_id: 1
-         })
+        const [name, setName] = useState("")
+        const [age, setAge] = useState(0)
+        const [password, setPassword] = useState("")
+        const [bio, setBio] = useState("")
+        const [org, setOrg] = useState(1)
+    
       
         function handleSubmit(e) {
           e.preventDefault();
@@ -19,37 +18,49 @@ function Login () {
             headers: {
               "Content-Type": "application/json",
             },
-            body: JSON.stringify({user}),
+            body: JSON.stringify({
+                name: name,
+                age: age,
+                password: password,
+                bio: bio,
+                organization_id: org, 
+            }),
           })
             .then((r) => r.json())
-            .then(user => console.log(user));
+            .then((res) => console.log(res));
         }
       
         return (
             <div className="App">
-            <form>
+            <form onSubmit={handleSubmit}>
               <div>
                 <input
                   name='name'
                   placeholder='Name'
-                  onChange={(e) => console.log(e.target.value)}
+                  onChange={(e) => setName(e.target.value)}
                 />
                  <input
                   name='password'
                   placeholder='Password'
-                  onChange={(e) => console.log(e.target.value)}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
                 <input
                   name='age'
                   placeholder='Age'
-                  onChange={(e) => console.log(e.target.value)}
+                  onChange={(e) => setAge(e.target.value)}
                 />
                  <input
                   name='bio'
                   placeholder='Tell us about you!'
-                  onChange={(e) => console.log(e.target.value)}
+                  onChange={(e) => setBio(e.target.value)}
+                />
+                 <input
+                  name='org'
+                  placeholder='Organization'
+                  onChange={(e) => setOrg(e.target.value)}
                 />
               </div>
+              <button type="submit">Create Account</button>
             </form>
           </div>
         );
