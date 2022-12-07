84d4a6a9b588d9a201347a98cb38c289f93abc20
veryharrywilz <110489443+veryharrywilz@users.noreply.github.com>
2022-12-06 20:24:08 -0500

    Added hover functionality and removed key error

--- client/src/LocationPin.js
+++ client/src/LocationPin.js
@@ -5,32 +5,34 @@ import MapJobCard from "./components/MapJobCard"
 // import loctag from "./images/LocationIcon.png"
 
 function LocationPin({ text, id }) {
-  const [displayJob, setDisplayJob] = useState(false)
+  const [displayJob, setDisplayJob] = useState(true)
   const [locationJob, setLocationJob] = useState([])
 
   useEffect(() => {
     fetch(`/locations/${id}`)
       .then((r) => r.json())
-      .then(data => {
-        setLocationJob(data.jobs)
-        console.log(locationJob)
-      })
+      .then(data => setLocationJob(data.jobs))
   }, []);
 
-  const jobArray = locationJob.map((job) => {
+  const jobArray = locationJob.map(job => {
     return (
-      <MapJobCard job={job}/>
+      <MapJobCard job={job} key={job.job_name}/>
     )
   })
 
   function handleHover() {
-    console.log(text)
+    setDisplayJob(false)
+  }
+
+  function handleLeave() {
+    setDisplayJob(true)
   }
 
   return (
-    <div className="pin" onMouseEnter={(e) => handleHover()}>
+    <div className="pin" onMouseEnter={(e) => handleHover()} onMouseLeave={(e) => handleLeave()}>
       <Icon icon={locationIcon} className="pin-icon" />
       <p className="pin-text">{text}</p>
+      {displayJob ? null : jobArray}
     </div>
   )
 }
