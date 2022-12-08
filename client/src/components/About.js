8e382a3c0e43e2b7a0fa12e4fbd0c32e7498b39b
angpace <108838753+angpace@users.noreply.github.com>
2022-12-08 14:53:46 -0500

    Hello

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,12 +1,16 @@
 function About () {
+    
 
     return (
-        <div className="content">
-        <h1> Hello, we're NYClean! </h1>
-        <img className="volunteers" src="https://washingtonsqpark.org/wp-content/uploads/2022/09/IMG_5583-1300x975.jpeg"></img>
-        <p>Our mission is to keep our city clean. Volunteer for clean up projects near you that your fellow city goers have posted.
-            Is there a mess in your neighborhood? Post a job and gather some of the greatest New Yorkers to help
-            you remedy this. </p>
+        <div className="content" id="homecontent">
+        <h1 className="welcome"> Hello, we're NYClean! </h1>
+            <div className="vol-container">
+                <img className="volunteers" src="https://washingtonsqpark.org/wp-content/uploads/2022/09/IMG_5583-1300x975.jpeg"></img>
+                <p className="about-text">Our mission is to keep our city clean. Volunteer for clean up projects near you that your fellow city goers have posted.
+                Is there a mess in your neighborhood? Post a job and gather some of the greatest New Yorkers to help
+                you remedy this. </p>
+                <p className="about-text">sihfosjdgoisueogn dgjhei orgioygsohg o sigh o s gisogishgohgosohgoj s gosug</p>
+            </div>
         </div>
     )
 }
