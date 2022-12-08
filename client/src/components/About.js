4beb1ee9dbf75251071e1e03c7a5d8d1222566dd
angpace <108838753+angpace@users.noreply.github.com>
2022-12-08 18:10:48 -0500

    Editing home page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,16 +1,18 @@
 function About () {
-    
 
     return (
-        <div className="content" id="homecontent">
-        <h1 className="welcome"> Hello, we're NYClean! </h1>
-            <div className="vol-container">
-                <img className="volunteers" src="https://washingtonsqpark.org/wp-content/uploads/2022/09/IMG_5583-1300x975.jpeg"></img>
-                <p className="about-text">Our mission is to keep our city clean. Volunteer for clean up projects near you that your fellow city goers have posted.
-                Is there a mess in your neighborhood? Post a job and gather some of the greatest New Yorkers to help
-                you remedy this. </p>
-                <p className="about-text">sihfosjdgoisueogn dgjhei orgioygsohg o sigh o s gisogishgohgosohgoj s gosug</p>
-            </div>
+        <div className="content">
+        <h1> Hello, we're NYClean! </h1>
+        <div className="flex">
+        <img className="volunteers" src="https://washingtonsqpark.org/wp-content/uploads/2022/09/IMG_5583-1300x975.jpeg"></img>
+        <p className="text">Our mission is to keep our city clean. Volunteer for clean up projects near you that your fellow city goers have posted.
+            <br/>
+            Our forests, rivers, oceans and soils provide us with the food we eat, the air we breathe, the water we irrigate our crops with. 
+            We also rely on them for numerous other goods and services we depend on for our health, happiness and prosperity. These natural assets are often called the world's 'natural capital'.
+            <br/>
+            Is there a mess in your neighborhood? Post a job and gather some of the greatest New Yorkers to help
+            you remedy this. </p>
+        </div>
         </div>
     )
 }
