5b62026e3790ff6f8c8ffcdb24ca4402da9e64d6
Keshav Bhati <keshavbhati2000@gmail.com>
2022-12-08 21:12:53 -0500

    big fixed

--- client/src/components/JobContainer.js
+++ client/src/components/JobContainer.js
@@ -2,17 +2,31 @@ import JobCard from "./JobCard";
 // import { useState, useEffect } from "react";
 import Search from "./Search";
 
-function JobContainer({ jobs, setSearch, user}) {
+function JobContainer({ jobs, setSearch, user }) {
+
+  if (!user) {
+    const renderJob = jobs.map((job) => {
+      return <JobCard job={job} user={user} key={job.id} />
+    })
+    return (
+      <>
+        <h1>View Jobs</h1>
+        <Search setSearch={setSearch} />
+        <div className="jobContainer">
+          {renderJob}
+        </div>
+      </>
+    )
+  }
 
 
- 
 
 
   const jobsArray = jobs.map(job => {
     const hasJob = user.volunteers.map(j => j.job_id)
-    const initialIsSignedUp = hasJob.includes(job.id) 
+    const initialIsSignedUp = hasJob.includes(job.id)
     // set then pass down, issignedupis set to that
-    return <JobCard job={job} key={job.id} user={user} initialIsSignedUp={initialIsSignedUp}/>
+    return <JobCard job={job} key={job.id} user={user} initialIsSignedUp={initialIsSignedUp} />
   })
 
 
