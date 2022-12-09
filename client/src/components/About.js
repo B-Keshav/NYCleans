3f190559d3c67be0b123a08b12f4f48070a20b2e
angpace <108838753+angpace@users.noreply.github.com>
2022-12-08 23:11:01 -0500

    Updates on Home

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,18 +1,50 @@
-function About () {
+import { useState } from "react";
+import Map from "../Map";
+
+function About ({user}) {
+    const [isTrue, setIsTrue] = useState(false)
+
+
+    function isItTrue () {
+        setIsTrue(!isTrue)
+        console.log(isTrue)
+    }
 
     return (
         <div className="content">
-        <h1> Hello, we're NYClean! </h1>
-        <div className="flex">
-        <img className="volunteers" src="https://washingtonsqpark.org/wp-content/uploads/2022/09/IMG_5583-1300x975.jpeg"></img>
-        <p className="text">Our mission is to keep our city clean. Volunteer for clean up projects near you that your fellow city goers have posted.
-            <br/>
-            Our forests, rivers, oceans and soils provide us with the food we eat, the air we breathe, the water we irrigate our crops with. 
-            We also rely on them for numerous other goods and services we depend on for our health, happiness and prosperity. These natural assets are often called the world's 'natural capital'.
-            <br/>
-            Is there a mess in your neighborhood? Post a job and gather some of the greatest New Yorkers to help
-            you remedy this. </p>
-        </div>
+            <h1> Hello, we're NYClean! </h1>
+                <div className="flex">
+                 <img className="volunteers" src="https://washingtonsqpark.org/wp-content/uploads/2022/09/IMG_5583-1300x975.jpeg"></img>
+                 <img className="volunteers1" src="https://assets.centralparknyc.org/media/images/_1650x767_crop_center-center_none/TeenVolunteerDay-20191019-01302.JPG"></img>
+                </div>
+                    <section className="pop">
+                        {isTrue?
+                             <h1 onClick={isItTrue} classsName="header">Click to Collapse</h1>
+                            :
+                            <h1 onClick={isItTrue} classsName="header">Click here to learn more about us!</h1>
+                        }
+                    </section>
+                        {isTrue? 
+                        <section  className="firstsection">
+                            <p>Our Mission <br/>Keeping our City Clean!</p>
+                            <p>Core Members <br/> 
+                                Madeline Dulchin <br/>
+                                Angela Pace<br/>
+                                Keshav Bhati<br/>
+                                Harrison Wilson<br/>
+                            </p>
+                            <p>Trees Planted:<br/>955,493</p>
+                        </section>
+                        :
+                        <section  className="firstsection">
+                            <p className="h1">Our Mission Statement </p>
+                            <p  className="h2">Our Core Team</p>
+                            <p  className="h3">Amount of Trees Planted!</p>
+                        </section>
+                    }
+            
+                    
+                <Map user={user}/>
         </div>
     )
 }
